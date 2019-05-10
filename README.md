79520de68b0694b919948bd9d7f31fd56a4b6fd2
Kry9toN <dhimasbagusprayoga@gmail.com>
2019-05-10 11:27:03 +0700

    add file

---
+++ README.md
@@ -0,0 +1,19 @@
+# Extra Key
+Extra Key Termux
+
+# Usage
+```bash
+$ pkg install python
+```
+```bash
+$ pkg install git
+```
+```bash
+$ git clone https://github.com/kry9ton/extrakey
+```
+```bash
+$ cd terkey
+```
+```bash
+$ python extrakey.py
+```
---
+++ extrakey.py
@@ -0,0 +1,36 @@
+import os
+from time import sleep
+
+
+a ='\033[92m'
+b ='\033[91m'
+c ='\033[0m'
+os.system('clear')
+print(a+'\t  Extra Key Termux')
+print(b+'\t    KryPtoN')
+print('\t https://t.me/Kry9toN')
+print(a+'+'*40)
+print('\nProses..')
+sleep(1)
+print(b+'\n[!] making termux properties directory..')
+sleep(1)
+try:
+      os.mkdir('/data/data/com.termux/files/home/.termux')
+except:
+      pass
+print(a+'[!]Success !')
+sleep(1)
+print(b+'\n[!] Making setup file..')
+sleep(1)
+
+key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
+control = open('/data/data/com.termux/files/home/.termux/termux.properties','w')
+control.write(key)
+control.close()
+sleep(1)
+print(a+'[!] Success !')
+sleep(1)
+print(b+'\n[!] Setting up..')
+sleep(2)
+os.system('termux-reload-settings')
+print(a+'[!] Successfully !!')
